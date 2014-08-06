80589f9bcb4ea5752ea6a6053b072494097ec6b2
Alyssa Batula <batulaa@drexel.edu>
2014-08-06 14:54:27 -0400

    Highlight 80th column in python-mode

---
+++ lisp/init-preload-local.el
@@ -0,0 +1,4 @@
+;;; Require column marker
+(require 'column-marker)
+
+(provide 'init-preload-local)
--- lisp/init-python-mode.el
+++ lisp/init-python-mode.el
@@ -3,5 +3,7 @@
 		("SConscript\\'" . python-mode))
               auto-mode-alist))
 
+;; Mark 80 character colum when working in Python
+(add-hook 'python-mode-hook (lambda () (interactive) (column-marker-1 80)))
 
 (provide 'init-python-mode)

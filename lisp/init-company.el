e245fae28b4f47b3abe719513670d7e8df33f4b4
Alyssa Batula <alyssa.batula@coxautoinc.com>
2019-03-19 16:20:04 -0400

    i don't think it's doing anything, but some stuff for company

---
+++ lisp/init-company.el
@@ -0,0 +1,10 @@
+;;; init-company.el --- Completion with company -*- lexical-binding: t -*-
+
+(require 'company)
+
+;; Bind auto-complete to a key
+(global-set-key (kbd "C-j") 'company-complete)
+
+
+(provide 'init-company)
+;;; init-company.el ends here
